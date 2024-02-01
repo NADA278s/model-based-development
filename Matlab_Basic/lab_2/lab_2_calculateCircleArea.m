f9efeb9af1db0d7e4905770836639b9acf6361ba
NADA278s <147089845+NADA278s@users.noreply.github.com>
2024-02-01 21:31:34 +0200

    Add files via upload

---
+++ Matlab_Basic/lab_2/lab_2_calculateCircleArea.m
@@ -0,0 +1,4 @@
+function area = lab_3_calculateCircleArea(diametar)
+area = pi * diametar^2;
+fprintf('area : %d', area)
+end 
---
+++ Matlab_Basic/lab_2/lab_2_mySimpleFunction.m
@@ -0,0 +1,4 @@
+function sum= lab_2_mySimpleFunction(num1,num2)
+sum = num1+num2;
+disp(sum)
+end
