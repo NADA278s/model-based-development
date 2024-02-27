7418778beb6f7a7d84f349fd84ada3c8767f4cbb
NADA278s <147089845+NADA278s@users.noreply.github.com>
2024-02-27 10:37:40 +0200

    Add files via upload

---
+++ sumlink_module/temparture_control/TEMPPARA.m
@@ -0,0 +1,7 @@
+% Define values for tau (time constant) and Ku (system gain)
+tau = 10;  % seconds
+Ku = 1;
+
+% Display the chosen values
+disp(['Time constant (tau): ', num2str(tau), ' seconds']);
+disp(['System gain (Ku): ', num2str(Ku)]);
